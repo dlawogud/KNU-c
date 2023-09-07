bb4de4ad59edc7e3d92bb6a2b874f663ec44f24d
dlawogud <144310637+dlawogud@users.noreply.github.com>
2023-09-07 15:49:13 +0900

    vlfkalem

--- Project1/Project1/FileName.cpp
+++ Project1/Project1/FileName.cpp
@@ -1,7 +1,26 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
-	printf("hello world\n");
+	int a;
+	int b;
+	int c;
+	int n;
+
+	scanf_s("%d", &n);
+
+
+	
+	for (a = 0; a < n; a++)
+	{
+		for (b = a; b <= n - 1; b++) {
+			printf(" ");
+		}
+		for (c = 0; c <= (a * 2); c++) {
+			printf("*");
+		}
+		printf("\n");
+
+	}
 
 	return 0;
 }
